1693df41a62292c6629a4de747866f3f8e1e8121
Brian Murphy <gurudvlp@gmail.com>
2017-06-18 12:51:27 -0400

    Added helper functions to xmpponent.Accounts.Contact

--- xmpponent/Accounts/Contact.cs
+++ xmpponent/Accounts/Contact.cs
@@ -12,6 +12,23 @@ namespace xmpponent.Accounts
 		public string JID
 		{ get { return _JID; } set { _JID = value; } }
 
+		private bool _LocalSubscription = false;
+		/// <summary>
+		/// Gets or sets a value indicating whether this account on the component has
+		/// a subscription to this <see cref="xmpponent.Accounts.Contact"/>.
+		/// </summary>
+		/// <value><c>true</c> if local subscription; otherwise, <c>false</c>.</value>
+		public bool LocalSubscription
+		{ get { return _LocalSubscription; } set { _LocalSubscription = value; } }
+
+		private bool _RemoteSubscription = false;
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="xmpponent.Accounts.Contact"/> has
+		/// a subscription to this account on the component.
+		/// </summary>
+		/// <value><c>true</c> if remote subscription; otherwise, <c>false</c>.</value>
+		public bool RemoteSubscription
+		{ get { return _RemoteSubscription; } set { _RemoteSubscription = value; } }
 
 		public Contact ()
 		{
